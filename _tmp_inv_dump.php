43b94d5a19596e9ba8cb98caaf338b0be38a8073
edzarabia118-bit <ed.zarabia118@gmail.com>
2026-03-06 05:09:12 -0600

    Delete _tmp_inv_dump.php

--- _tmp_inv_dump.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once __DIR__ . '/conexion/conexion.php';
-$res = $conn->query("SELECT id,nombre,stock,unidad FROM inventario ORDER BY id ASC LIMIT 200");
-while($r = $res->fetch_assoc()) {
-    echo $r['id'] . ' | ' . $r['nombre'] . ' | ' . $r['stock'] . ' | ' . $r['unidad'] . PHP_EOL;
-}
